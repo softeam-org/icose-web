dda41a4d651682495be57521384b8f5dca3a737e
Carlos Adriano <cadrianogamac@gmail.com>
2023-05-18 17:59:28 -0300

    Renderiza o cabeçalho na tela

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Header } from "./components/Header"
+
 function App() {
   return (
-    <div className="flex min-h-screen justify-center items-center">
-      <div className="text-8xl">Hello, world!</div>
+    <div>
+      <Header/>
     </div>
   ) 
 }
