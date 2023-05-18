0adefe516155f406ca79a71635999781c8ad0148
Carlos Adriano <cadrianogamac@gmail.com>
2023-05-18 16:45:54 -0300

    Adiciona azul customizado ao tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'standard-blue' : '#1E3A8A'
+      }
+    },
   },
   plugins: [],
 }
