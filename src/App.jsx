d67295ebc2f71f96820197c2e926a5d71678ee1b
Carlos Adriano <cadrianogamac@gmail.com>
2023-05-22 09:25:37 -0300

    Altera modo de exportação do módulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import  Footer  from "./components/Footer"
-import { Header } from "./components/Header"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
 
 function App() {
   return (
@@ -10,4 +10,5 @@ function App() {
   ) 
 }
 
-export default App
+export default App;
+
