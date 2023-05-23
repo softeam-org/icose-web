7b93ff3d9db1c97a8a44164d9f6fb6b0b303f8f4
Carlos Adriano <cadrianogamac@gmail.com>
2023-05-23 15:05:02 -0300

    Cria componente para cada tela do site

---
+++ src/Pages/Incio.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="flex-1 flex justify-center items-center text-5xl">
+      <div> Work in progress... 🚧🏗️ </div>
+    </div>
+  );
+}
+
+export default Home;
+
---
+++ src/Pages/Sobre.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div className="flex-1 flex justify-center items-center text-5xl">
+      <div> Work in progress... 🚧🏗️ </div>
+    </div>
+  );
+}
+
+export default About;
+
