e2b7e2c1b48de30966941e18f5b741ca73dfb671
Carlos Adriano <cadrianogamac@gmail.com>
2023-05-25 17:12:11 -0300

    Cria arquivo contendo dados sobre as rotas do site

---
+++ data/pages.js
@@ -0,0 +1,33 @@
+export const pages = [
+  {
+    name: "Início",
+    route: "/",
+    filename: "Inicio",
+  },
+  {
+    name: "Sobre",
+    route: "/sobre",
+    filename: "Sobre",
+  },
+  {
+    name: "Editais",
+    route: "/editais",
+    filename: "Editais",
+  },
+  {
+    name: "Transferências",
+    route: "/transferencias",
+    filename: "Transferencias",
+  },
+  {
+    name: "Parceiros",
+    route: "/parceiros",
+    filename: "Parceiros",
+  },
+  {
+    name: "Doe Aqui",
+    route: "/doacao",
+    filename: "Doacao",
+  },
+];
+
