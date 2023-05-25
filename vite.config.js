ae0165706d6221d0c1778f5589419b317182932f
Carlos Adriano <cadrianogamac@gmail.com>
2023-05-25 17:17:06 -0300

    Cria alias para diretório data

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ import path from 'path';
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, '/src'),
+      'data': path.resolve(__dirname, '/data'),
     },
   },
   plugins: [react()],
