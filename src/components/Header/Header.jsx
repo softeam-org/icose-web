a4f3e4e5ecbe73867bf26983e2eff60493795373
Carlos Adriano <cadrianogamac@gmail.com>
2023-05-29 20:24:57 -0300

    Aumenta z-index do header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import HeaderLogo from "@/components/Header/Logo";
 
 function Header() {
   return (
-    <div className="sticky inset-x-0 top-0 bg-standard-blue">
+    <div className="sticky z-50 inset-x-0 top-0 bg-standard-blue">
       <div className="mx-12 py-7 flex gap-4 justify-between items-center">
         <HeaderLogo />
         <NavBar />
