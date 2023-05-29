b68ad678356df8b7d7eec920eadfcd7a15e08f67
Carlos Adriano <cadrianogamac@gmail.com>
2023-05-29 20:28:52 -0300

    Adiciona cards à tela principal

--- src/Pages/Inicio.jsx
+++ src/Pages/Inicio.jsx
@@ -1,7 +1,25 @@
+import Card from "@/components/Inicio/Card"
+import { Target } from "@phosphor-icons/react";
+import { Heart } from "@phosphor-icons/react";
+
 function Home() {
   return (
-    <div className="flex-1">
+    <div className="flex-1 bg-background-section">
       <div className="w-full h-0 pt-[50%] bg-map bg-no-repeat bg-cover bg-center xl:p-0 xl:h-[50rem]"></div>
+      <div className="mx-44">
+        <div className="flex gap-16 my-28 flex-col xl:flex-row">
+          <Card 
+            icon={<Target size={32} />}
+            title={"NOSSA MISSÃO"}
+            text={"Promover o fortalecimento do protagonismo das comunidades locais e a atuação em rede, visando o desenvolvimento justo, solidário e sustentável de Sergipe."}
+          />
+          <Card 
+            icon={<Heart size={32} weight="fill" />}
+            title={"NOSSOS VALORES"}
+            text={"Ética, solidariedade, inovação, transparência e comprometimento"}
+          />
+        </div>
+      </div>
     </div>
   );
 }
