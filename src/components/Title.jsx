3364bab710c713eb4bfd1bca93c9640271b0bc5d
Carlos Adriano <cadrianogamac@gmail.com>
2023-05-31 11:49:07 -0300

    Conserta bug na definição das cores do sublinhado

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,12 +1,34 @@
-function Title({underlineColor, centeredTitle, content}) {
-  const underlineColorCss = `after:bg-${underlineColor ? `[${underlineColor}]` : "standard-blue"}`;
-  const underlinePosition = centeredTitle ? "flex-col" : "";
+import resolveConfig from "tailwindcss/resolveConfig";
+import tailwindConfig from "~/tailwind.config.js"
 
-  return (
-    <div className={`${underlineColorCss} ${underlinePosition} flex font-bold text-2xl relative items-center after:content-[''] after:absolute after:bottom-[-0.7rem] after:w-12 after:h-[6px]`}>
+function Title({color, centeredTitle, content}) {
+  const underlinePosition = centeredTitle ? "flex-col" : "";
+  const underlineColor = setunderlineColor(color);
+
+  return ( 
+    <div className={`${underlinePosition} flex font-bold text-2xl relative items-center`}>
       {content}
+      <span 
+        style={{ backgroundColor: `${underlineColor}`}}
+        className="absolute bottom-[-0.7rem] w-12 h-[6px]" 
+      />
     </div>
   );
+}
+
+function setunderlineColor(color) {
+  const colors = resolveConfig(tailwindConfig).theme.colors;
+  const hexRegex = new RegExp(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/);
+
+  const isHex = hexRegex.test(color);
+
+  if (isHex)
+    return color;
+
+  if (!color || !colors[color])
+    return colors["standard-blue"];
+
+  return colors[color];
 }
 
 export default Title;
