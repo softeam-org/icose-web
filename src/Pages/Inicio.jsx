2fff5bc042f954e5853b46d0a7ac51e45bf5fbcf
Carlos Adriano <cadrianogamac@gmail.com>
2023-05-31 12:58:51 -0300

    Cria responsividade da imagem do mapa de Sergipe

--- src/Pages/Inicio.jsx
+++ src/Pages/Inicio.jsx
@@ -1,11 +1,16 @@
-import Card from "@/components/Inicio/Card"
 import { Target } from "@phosphor-icons/react";
 import { Heart } from "@phosphor-icons/react";
+import Card from "@/components/Inicio/Card"
+import map from "@/assets/map.svg"
 
 function Home() {
   return (
     <div className="flex-1 bg-background-section">
-      <div className="w-full h-0 pt-[50%] bg-map bg-no-repeat bg-cover bg-center xl:p-0 xl:h-[50rem]"></div>
+      <img 
+        className="object-cover w-[640px] h-[320px] sm:w-[1024px] sm:h-[512px] lg:w-[1280px] lg:h-[640px] xl:w-full xl:h-[50rem]"
+        src={map}
+        alt="Mapa de Sergipe"
+      />
       <div className="mx-auto max-w-[110rem] px-12">
         <div className="flex gap-10 xl:gap-16 my-12 xl:my-28 flex-col min-[800px]:flex-row">
           <Card 
