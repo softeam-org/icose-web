f366c5ebde54ebc207deb5cad7071dbc2a6e56e1
Carlos Adriano <cadrianogamac@gmail.com>
2023-06-01 12:45:07 -0300

    Cria a aba "Mapeamento"

--- src/Pages/Inicio.jsx
+++ src/Pages/Inicio.jsx
@@ -3,6 +3,8 @@ import Card from "@/components/Inicio/Card"
 import PlusButton from "@/components/Inicio/PlusButton";
 import Title from "@/components/Title"
 import map from "@/assets/map.svg"
+import businessWoman from "@/assets/business-woman.svg"
+import workingWoman from "@/assets/working-woman.png"
 
 function Home() {
   return (
@@ -14,7 +16,7 @@ function Home() {
       />
 
       <section className="bg-background-section">
-        <div className="flex flex-col gap-10 xl:gap-20 py-10 xl:py-20 mx-auto max-w-[100rem] px-12">
+        <div className="flex flex-col gap-10 xl:gap-20 pb-10 pt-10 xl:pt-20 mx-auto max-w-[100rem] px-12">
           <div className="flex gap-8 flex-col xl:gap-16 min-[800px]:flex-row">
             <Card
               icon={<Target size={32} />}
@@ -28,15 +30,15 @@ function Home() {
             />
           </div>
 
-          <div className="flex flex-col gap-6 xl:gap-8">
+          <div className="flex flex-col gap-6 xl:gap-8 text-2xl">
             <Title 
               color="standard-blue"
               content="Conheça o Icose:"
             /> 
-            <p className="font-medium text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum. Phasellus eros erat, tristique id felis eget, dignissim malesuada mauris. Quisque a justo diam. Phasellus in vestibulum risus. Nunc felis dui, bibendum in tortor eu, tincidunt viverra justo. Nullam pellentesque leo eget tristique efficitur.</p>
+            <p className="font-medium">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum. Phasellus eros erat, tristique id felis eget, dignissim malesuada mauris. Quisque a justo diam. Phasellus in vestibulum risus. Nunc felis dui, bibendum in tortor eu, tincidunt viverra justo. Nullam pellentesque leo eget tristique efficitur.</p>
             <div className="flex flex-col xl:flex-row gap-8 xl:gap-10">
               <video className="flex-1" width="" height="" controls></video>
-              <div className="flex-1 flex flex-col gap-4 text-2xl">
+              <div className="flex-1 flex flex-col gap-4">
                 <h4 className="font-bold">Fortalecimento comunitário</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum. Phasellus eros erat, tristique id felis eget, dignissim malesuada mauris. </p>
                 <div className="flex flex-col gap-4">
@@ -45,8 +47,8 @@ function Home() {
                 </div>
               </div> 
             </div>
-            <p className="font-medium text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum. Phasellus eros erat, tristique id felis eget, dignissim malesuada mauris. Quisque a justo diam. Phasellus in vestibulum risus. Nunc felis dui, bibendum in tortor eu, tincidunt viverra justo. Nullam pellentesque leo eget tristique efficitur.</p>
-            <p className="font-medium text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum. Phasellus eros erat, tristique id felis eget, dignissim malesuada mauris. Quisque a justo diam. Phasellus in vestibulum risus. Nunc felis dui, bibendum in tortor eu, tincidunt viverra justo. Nullam pellentesque leo eget tristique efficitur.</p>
+            <p className="font-medium">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum. Phasellus eros erat, tristique id felis eget, dignissim malesuada mauris. Quisque a justo diam. Phasellus in vestibulum risus. Nunc felis dui, bibendum in tortor eu, tincidunt viverra justo. Nullam pellentesque leo eget tristique efficitur.</p>
+            <p className="font-medium">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum. Phasellus eros erat, tristique id felis eget, dignissim malesuada mauris. Quisque a justo diam. Phasellus in vestibulum risus. Nunc felis dui, bibendum in tortor eu, tincidunt viverra justo. Nullam pellentesque leo eget tristique efficitur.</p>
           </div>
 
           <div className="flex flex-col gap-4 items-center text-2xl font-bold">
@@ -60,6 +62,50 @@ function Home() {
                 <Phone size={32} />
                 <p className="text-center">(79) 3223-2159 (Para recados)</p>
               </span>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="flex flex-col gap-10 xl:gap-20 pt-10 pb-10 xl:pb-20 mx-auto max-w-[100rem] px-12">
+          <div className="flex flex-col gap-16 text-2xl">
+            <div className="flex flex-col gap-6 xl:gap-8">
+              <Title 
+                color="dark-green"
+                content="MAPEAMENTO:"
+              />
+              <p>Para identificar o potencial e as necessidades, estamos realizando o mapeamento das ONGs e iniciativas individuais no estado de Sergipe. Estas informações serão essenciais para entender as reais necessidades de cada um de vocês e assim construirmos juntos um caminho de fortalecimento mútuo. Participe!</p>
+            </div>
+
+            <div className="flex flex-col lg:flex-row gap-16">
+              <div className="pl-10 pt-10 self-center">
+                <img 
+                  className="shadow-[-2.5rem_-2.5rem] shadow-dark-green w-full lg:w-auto max-w-md"
+                  src={workingWoman}
+                  alt="Mulher trabalhando em serviço comunitário"
+                />
+              </div>
+              <div className="flex flex-col gap-4">
+                <h4 className="font-bold">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h4>
+                <p>Vestibulum tempor condimentum nisl, et rhoncus lorem faucibus vel. Donec eu lectus id tortor lobortis aliquam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum lobortis purus sed tellus aliquam accumsan.</p>
+                <button className="text-white font-bold px-3 py-4 bg-dark-green rounded-xl self-center">Acessar</button>
+              </div>
+            </div>
+
+            <div className="flex flex-col lg:flex-row-reverse gap-16">
+              <div className="pr-10 pb-10 self-center">
+                <img 
+                  className="shadow-[2.5rem_2.5rem] shadow-dark-green w-full lg:w-auto max-w-md"
+                  src={businessWoman}
+                  alt="Mulher sorrindo"
+                />
+              </div>
+              <div className="flex flex-col gap-4">
+                <h4 className="font-bold">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h4>
+                <p>Vestibulum tempor condimentum nisl, et rhoncus lorem faucibus vel. Donec eu lectus id tortor lobortis aliquam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum lobortis purus sed tellus aliquam accumsan.</p>
+                <button className="text-white font-bold px-3 py-4 bg-dark-green rounded-xl self-center">Acessar</button>
+              </div>
             </div>
           </div>
         </div>
