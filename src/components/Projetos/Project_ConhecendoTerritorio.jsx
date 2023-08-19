625ce807606642924a756be64509b6a9324a3e58
Daniel Sena <danielsan.2000@hotmail.com>
2023-08-19 11:24:27 -0300

    Cria componentes para cada Projeto

---
+++ src/components/Projetos/Project_ConhecendoTerritorio.jsx
@@ -0,0 +1,20 @@
+function Project_ConhecendoTerritorio() {
+  return (
+    <div>
+      <h2 className="font-bold">Projeto Conhecendo o Território</h2>
+      <p> 
+        Para identificar o potencial e as necessidades, estamos realizando o mapeamento das ONG e iniciativas individuais no estado de Sergipe. Estas informações serão essenciais para a construção de futuros editais e repasse de verbas. 
+        <br /> 
+        Participe! 
+      </p>
+      <br />
+      <h3>Formulários</h3>
+      <ul className= "list-disc px-12">
+        <li>ONG</li>
+        <li>Projetos individuais</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Project_ConhecendoTerritorio;
