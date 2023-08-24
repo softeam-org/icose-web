bcb795f6bca009a2c9204b15e384b81672e6a50a
Daniel Sena <danielsan.2000@hotmail.com>
2023-08-23 22:37:02 -0300

    Adiciona flex-1 e corrige padding

--- src/Pages/Projetos.jsx
+++ src/Pages/Projetos.jsx
@@ -3,8 +3,8 @@ import ProjectRender from "@/components/Projects/ProjectRender";
 
 function Projects() {
   return (
-    <section className="bg-background-section ">
-      <div className="flex flex-col gap-8 xl:gap-8 pb-10 pt-10 xl:pt-20 xl:pb-20 mx-auto max-w-[100rem] px-20">
+    <section className="bg-background-section flex-1">
+      <div className="flex flex-col gap-8 xl:gap-8 pb-10 pt-10 xl:pt-20 xl:pb-20 mx-auto max-w-[100rem] px-12">
         <h1>
           <Title 
             color="standard-blue" 
