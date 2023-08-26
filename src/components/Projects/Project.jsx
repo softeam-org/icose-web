c3b4875fa970f1a3fc6417f39832bbc17366f0e1
Carlos Adriano <cadrianogamac@gmail.com>
2023-08-26 11:11:51 -0300

    Refatora código da aba projetos

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,15 +1,10 @@
-import { lazy, Suspense } from "react";
-import Loading from "@/components/Loading";
-
-function Project({ title: projectTitle , file: projectFileName }) {
+function Project({ title, content }) {
  
-  const ProjectContent = lazy(() => import(`./Contents/${projectFileName}.jsx`));
-
   return (
-   <Suspense fallback={<Loading />}>
-      <h2 className="font-bold">{projectTitle}</h2>
-      <ProjectContent />
-    </Suspense>
+    <div className="flex flex-col gap-2">
+      <h1 className="text-lg font-bold">{title}</h1>
+      {content}
+    </div>
   )
 }
 
