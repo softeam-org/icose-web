46f495adbec12437d06566c1920042f34ffcd552
Carlos Adriano <cadrianogamac@gmail.com>
2023-08-26 11:28:39 -0300

    Melhora responsividade da frase

--- src/components/UnderConstruction.jsx
+++ src/components/UnderConstruction.jsx
@@ -1,6 +1,6 @@
 function UnderConstruction() {
   return (
-    <div className="flex-1 flex justify-center items-center text-5xl flex-col gap-8 xl:gap-8 pb-10 pt-10 xl:pt-20 xl:pb-20 mx-auto max-w-[100rem] px-12">
+    <div className="flex-1 flex justify-center items-center flex-col gap-8 xl:gap-8 text-4xl sm:text-5xl py-40 xl:py-20 mx-auto max-w-[100rem] px-12">
       <div> Em construção... 🚧🏗️ </div>
     </div>
   );
