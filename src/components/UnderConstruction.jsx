4e7238153fa4fec4b8d619c54ef357bd96d4b388
Carlos Adriano <cadrianogamac@gmail.com>
2023-08-26 11:25:26 -0300

    Corrige typo

--- src/components/UnderConstruction.jsx
+++ src/components/UnderConstruction.jsx
@@ -1,7 +1,7 @@
 function UnderConstruction() {
   return (
     <div className="flex-1 flex justify-center items-center text-5xl flex-col gap-8 xl:gap-8 pb-10 pt-10 xl:pt-20 xl:pb-20 mx-auto max-w-[100rem] px-12">
-      <div> Em Construção ... 🚧🏗️ </div>
+      <div> Em construção... 🚧🏗️ </div>
     </div>
   );
 }
