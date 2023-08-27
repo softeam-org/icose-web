bdff1f1887d11e7a58a0a4e217f571103f441d5f
Jeferson-Dcomp <jefersondeoliveira062@gmail.com>
2023-08-26 23:27:52 -0300

    resolve problemas no carrosel

--- src/components/Inicio/Carousel.jsx
+++ src/components/Inicio/Carousel.jsx
@@ -1,238 +1,100 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 function Carousel() {
-	const [selectedButton, setSelectedButton] = useState(1);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [data, setData] = useState([]);
+  const carousel = useRef(null);
 
-	const handleButtonClick = (index) => {
-		setSelectedButton(index);
-	};
-	return (
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5173/data/news.js');
+        const responseData = await response.json();
+        setData(responseData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-			<div>
-				<div className="carousel  w-full flex py-8 space-x-10">
-					<div id="item1" className="flex flex-row carousel-item w-full space-x-10">
-						<div className="w-1/3 ">
-							<img src="src\assets\news.svg" className="w-full" />
-							<div className="w-full flex flex-col space-y-1.5 md:space-y-2 py-3 ">
-								<h2 className="font-inter font-semibold text-standard-blue text-xs sm:text-base md:text-2xl xl:text-2xl text-left">
-									Título da matéria
-								</h2>
-								<p className="text-xs">5 de fevereiro de 2022</p>
-								<p className="text-xs sm:text-sm md:text-base w-full h-full ">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum...
-								</p>
-							</div>
-						</div>
-						<div className="w-1/3 ">
-							<img src="src\assets\news.svg" className="w-full" />
-							<div className="w-full flex flex-col space-y-1.5 md:space-y-2 py-3 ">
-								<h2 className="font-inter font-semibold text-standard-blue text-xs sm:text-base md:text-2xl xl:text-2xl text-left">
-									Título da matéria
-								</h2>
-								<p className="text-xs">5 de fevereiro de 2022</p>
-								<p className="text-xs sm:text-sm md:text-base w-full h-full ">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum...
-								</p>
-							</div>
-						</div>
-						<div className="w-1/3 ">
-							<img src="src\assets\news.svg" className="w-full" />
-							<div className="w-full flex flex-col space-y-1.5 md:space-y-2 py-3 ">
-								<h2 className="font-inter font-semibold text-standard-blue text-xs sm:text-base md:text-2xl xl:text-2xl text-left">
-									Título da matéria
-								</h2>
-								<p className="text-xs">5 de fevereiro de 2022</p>
-								<p className="text-xs sm:text-sm md:text-base w-full h-full ">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum...
-								</p>
-							</div>
-						</div>
-					</div>
-					<div id="item2" className="flex flex-row carousel-item w-full space-x-10">
-						<div className="w-1/3 ">
-							<img src="src\assets\news.svg" className="w-full" />
-							<div className="w-full flex flex-col space-y-1.5 md:space-y-2 py-3 ">
-								<h2 className="font-inter font-semibold text-standard-blue text-xs sm:text-base md:text-2xl xl:text-2xl text-left">
-									Título da matéria
-								</h2>
-								<p className="text-xs">5 de fevereiro de 2022</p>
-								<p className="text-xs sm:text-sm md:text-base w-full h-full ">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum...
-								</p>
-							</div>
-						</div>
-						<div className="w-1/3 ">
-							<img src="src\assets\news.svg" className="w-full" />
-							<div className="w-full flex flex-col space-y-1.5 md:space-y-2 py-3 ">
-								<h2 className="font-inter font-semibold text-standard-blue text-xs sm:text-base md:text-2xl xl:text-2xl text-left">
-									Título da matéria
-								</h2>
-								<p className="text-xs">5 de fevereiro de 2022</p>
-								<p className="text-xs sm:text-sm md:text-base w-full h-full ">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum...
-								</p>
-							</div>
-						</div>
-						<div className="w-1/3 ">
-							<img src="src\assets\news.svg" className="w-full" />
-							<div className="w-full flex flex-col space-y-1.5 md:space-y-2 py-3 ">
-								<h2 className="font-inter font-semibold text-standard-blue text-xs sm:text-base md:text-2xl xl:text-2xl text-left">
-									Título da matéria
-								</h2>
-								<p className="text-xs">5 de fevereiro de 2022</p>
-								<p className="text-xs sm:text-sm md:text-base w-full h-full ">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum...
-								</p>
-							</div>
-						</div>
-					</div>
-					<div id="item3" className="flex flex-row carousel-item w-full space-x-10">
-						<div className="w-1/3 ">
-							<img src="src\assets\news.svg" className="w-full" />
-							<div className="w-full flex flex-col space-y-1.5 md:space-y-2 py-3 ">
-								<h2 className="font-inter font-semibold text-standard-blue text-xs sm:text-base md:text-2xl xl:text-2xl text-left">
-									Título da matéria
-								</h2>
-								<p className="text-xs">5 de fevereiro de 2022</p>
-								<p className="text-xs sm:text-sm md:text-base w-full h-full ">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum...
-								</p>
-							</div>
-						</div>
-						<div className="w-1/3 ">
-							<img src="src\assets\news.svg" className="w-full" />
-							<div className="w-full flex flex-col space-y-1.5 md:space-y-2 py-3 ">
-								<h2 className="font-inter font-semibold text-standard-blue text-xs sm:text-base md:text-2xl xl:text-2xl text-left">
-									Título da matéria
-								</h2>
-								<p className="text-xs">5 de fevereiro de 2022</p>
-								<p className="text-xs sm:text-sm md:text-base w-full h-full ">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum...
-								</p>
-							</div>
-						</div>
-						<div className="w-1/3 ">
-							<img src="src\assets\news.svg" className="w-full" />
-							<div className="w-full flex flex-col space-y-1.5 md:space-y-2 py-3 ">
-								<h2 className="font-inter font-semibold text-standard-blue text-xs sm:text-base md:text-2xl xl:text-2xl text-left">
-									Título da matéria
-								</h2>
-								<p className="text-xs">5 de fevereiro de 2022</p>
-								<p className="text-xs sm:text-sm md:text-base w-full h-full ">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum...
-								</p>
-							</div>
-						</div>
-					</div>
-					<div id="item4" className="flex flex-row carousel-item w-full space-x-10">
-						<div className="w-1/3 ">
-							<img src="src\assets\news.svg" className="w-full" />
-							<div className="w-full flex flex-col space-y-1.5 md:space-y-2 py-3 ">
-								<h2 className="font-inter font-semibold text-standard-blue text-xs sm:text-base md:text-2xl xl:text-2xl text-left">
-									Título da matéria
-								</h2>
-								<p className="text-xs">5 de fevereiro de 2022</p>
-								<p className="text-xs sm:text-sm md:text-base w-full h-full ">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum...
-								</p>
-							</div>
-						</div>
-						<div className="w-1/3 ">
-							<img src="src\assets\news.svg" className="w-full" />
-							<div className="w-full flex flex-col space-y-1.5 md:space-y-2 py-3 ">
-								<h2 className="font-inter font-semibold text-standard-blue text-xs sm:text-base md:text-2xl xl:text-2xl text-left">
-									Título da matéria
-								</h2>
-								<p className="text-xs">5 de fevereiro de 2022</p>
-								<p className="text-xs sm:text-sm md:text-base w-full h-full ">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum...
-								</p>
-							</div>
-						</div>
-						<div className="w-1/3 ">
-							<img src="src\assets\news.svg" className="w-full" />
-							<div className="w-full flex flex-col space-y-1.5 md:space-y-2 py-3 ">
-								<h2 className="font-inter font-semibold text-standard-blue text-xs sm:text-base md:text-2xl xl:text-2xl text-left">
-									Título da matéria
-								</h2>
-								<p className="text-xs">5 de fevereiro de 2022</p>
-								<p className="text-xs sm:text-sm md:text-base w-full h-full ">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum...
-								</p>
-							</div>
-						</div>
-					</div>
-					<div id="item5" className="flex flex-row carousel-item w-full space-x-10">
-						<div className="w-1/3 ">
-							<img src="src\assets\news.svg" className="w-full" />
-							<div className="w-full flex flex-col space-y-1.5 md:space-y-2 py-3 ">
-								<h2 className="font-inter font-semibold text-standard-blue text-xs sm:text-base md:text-2xl xl:text-2xl text-left">
-									Título da matéria
-								</h2>
-								<p className="text-xs">5 de fevereiro de 2022</p>
-								<p className="text-xs sm:text-sm md:text-base w-full h-full ">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum...
-								</p>
-							</div>
-						</div>
-						<div className="w-1/3 ">
-							<img src="src\assets\news.svg" className="w-full" />
-							<div className="w-full flex flex-col space-y-1.5 md:space-y-2 py-3 ">
-								<h2 className="font-inter font-semibold text-standard-blue text-xs sm:text-base md:text-2xl xl:text-2xl text-left">
-									Título da matéria
-								</h2>
-								<p className="text-xs">5 de fevereiro de 2022</p>
-								<p className="text-xs sm:text-sm md:text-base w-full h-full ">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum...
-								</p>
-							</div>
-						</div>
-						<div className="w-1/3 ">
-							<img src="src\assets\news.svg" className="w-full" />
-							<div className="w-full flex flex-col space-y-1.5 md:space-y-2 py-3 ">
-								<h2 className="font-inter font-semibold text-standard-blue text-xs sm:text-base md:text-2xl xl:text-2xl text-left">
-									Título da matéria
-								</h2>
-								<p className="text-xs">5 de fevereiro de 2022</p>
-								<p className="text-xs sm:text-sm md:text-base w-full h-full ">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum...
-								</p>
-							</div>
-						</div>
-					</div>
+    fetchData();
+  }, []);
 
+  useEffect(() => {
+// Here is the logic to get the scroll size
+    if (carousel.current) {
+      carousel.current.scrollLeft = currentIndex * carousel.current.offsetWidth;
+    }
+  }, [currentIndex]);
+
+//Here the scroll position is set and passed as index. In this way it is possible to assign the conditional of the labels.
+  const handleCarouselScroll = () => {
+    if (carousel.current) {
+      const scrollPosition = carousel.current.scrollLeft;
+      const itemWidth = carousel.current.offsetWidth;
+      const newIndex = Math.round(scrollPosition / itemWidth);
+      setCurrentIndex(newIndex);
+    }
+  };
+
+  if (!data || !data.length) return null;
+
+// Here is the logic next on the slide and previous on the slides
+  const handleLeftClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft -= carousel.current.offsetWidth;
+  };
+
+  const handleRightClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft += carousel.current.offsetWidth;
+  };
+
+  return (
+    <div className='flex flex-col'>
+      <div className='flex flex-row justify-center items-center space-x-5'>
+			<div className='w-10'>
+					<img src="src\assets\arrow-circle-left.svg" alt="arrow-right" onClick={handleLeftClick} className='w-8 hover:w-9' />
 				</div>
-
-				<div className="flex justify-center w-full h-2  gap-2">
-					<a href="#item1"
-						onClick={() => handleButtonClick(1)}
-						className={`btn w-1 p-1 h-0 min-h-0  rounded-full ${selectedButton === 1 ? 'bg-black' : 'border-solid  border-black'
-							}`}
-					></a>
-					<a href="#item2"
-						onClick={() => handleButtonClick(2)}
-						className={`btn w-1 p-1 h-0 min-h-0  rounded-full ${selectedButton === 2 ? 'bg-black' : 'border-solid  border-black'
-							}`}
-					></a>
-					<a href="#item3"
-						onClick={() => handleButtonClick(3)}
-						className={`btn w-1 p-1 h-0 min-h-0  rounded-full ${selectedButton === 3 ? 'bg-black' : 'border-solid  border-black'
-							}`}
-					></a>
-					<a href="#item4"
-						onClick={() => handleButtonClick(4)}
-						className={`btn w-1 p-1 h-0 min-h-0  rounded-full  ${selectedButton === 4 ? 'bg-black' : 'border-solid  border-black'
-							}`}
-					></a>
-					<a href="#item5"
-						onClick={() => handleButtonClick(5)}
-						className={`btn w-1 p-1 h-0 min-h-0 rounded-full  ${selectedButton === 5 ? 'bg-black ' : 'border-solid  border-black'
-							}`}
-					></a>
+        <div className="carousel carousel-center w-10/12 flex py-8 space-x-10" ref={carousel} onScroll={handleCarouselScroll}>
+          {data.map((slide, slideIndex) => (
+            <div id={slide.id} className="flex flex-row carousel-item w-full space-x-10" key={slideIndex}>
+              {slide.cards.map((card, cardIndex) => (
+                <div className="w-1/3 " key={cardIndex}>
+                  <img src={card.image} className="w-full" />
+                  <div className="w-full flex flex-col space-y-1.5 md:space-y-2 py-3 ">
+                    <h2 className="font-inter font-semibold text-standard-blue text-xs sm:text-base md:text-2xl xl:text-2xl text-left">
+                      {card.title}
+                    </h2>
+                    <p className="text-xs">{card.date}</p>
+                    <p className="text-xs sm:text-sm md:text-base w-full h-full ">
+                      {card.content}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+				<div className='w-10'>
+					<img src="src\assets\arrow-circle-right.svg" alt="arrow-right" onClick={handleRightClick} className='w-8 hover:w-9' />
 				</div>
-			</div>
+      </div>
 
+      {/* Radio buttons */}
+      <div className="flex justify-center w-full h-2 gap-2">
+        {data.map((_, slideIndex) => (
+          <span
+            key={slideIndex}
+            htmlFor={`radio${slideIndex}`}
+            className={`btn w-1 p-1 h-0 min-h-0 rounded-full ${
+              currentIndex === slideIndex ? 'bg-black' : 'border-solid border-black'
+            }`}
+          >
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-	);
-}
 export default Carousel;
