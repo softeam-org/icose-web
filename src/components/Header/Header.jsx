309b5c10e59fbe77d8e0872e77d9c06ba5ce7e02
Jorge Mendonça Boaretto Loch <jorgembloch2001@gmail.com>
2023-08-28 14:48:20 -0300

    Modifica padding do headder

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import HeaderLogo from "@/components/Header/Logo";
 function Header() {
   return (
     <div className="sticky z-50 inset-x-0 top-0 bg-standard-blue">
-      <div className="mx-12 py-4 xl:py-6 flex gap-4 justify-between items-center">
+      <div className="sm:mx-12 mx-6 py-3 flex gap-4 justify-between items-center">
         <HeaderLogo />
         <NavBar />
       </div>
