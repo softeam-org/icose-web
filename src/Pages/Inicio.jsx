0042a059abf2f6fb9a22f18b23954223460a181d
Uneto47 <netovasconcelos02@gmail.com>
2023-08-30 10:37:20 -0300

    Atualizando a pagina Inicio

--- src/Pages/Inicio.jsx
+++ src/Pages/Inicio.jsx
@@ -8,17 +8,19 @@ import News from "@/components/Inicio/News"
 function Home() {
   return (
     <div className="flex-1 text-2xl">
-      <img 
+      <img
         className="object-cover w-[640px] h-[320px] sm:w-[1024px] sm:h-[512px] lg:w-[1280px] lg:h-[640px] xl:w-full xl:h-[50rem]"
         src={map}
         alt="Mapa de Sergipe"
       />
 
       <section className="bg-background-section">
+        <h2 className="flex w-full pt-20 font-semibold text-xl justify-center">
+          Conheça a Icose
+        </h2>
         <div className="flex flex-col gap-10 xl:gap-20 pb-10 pt-10 xl:pt-20 mx-auto max-w-[100rem] px-12">
           <Cards />
           <Presentation />
-          <ContactUs />
         </div>
       </section>
 
@@ -29,7 +31,7 @@ function Home() {
       </section>
 
       <section className="flex justify-center flex-col	bg-background-section items-center py-8">
-        <News/>
+        <News />
       </section>
     </div>
   );
