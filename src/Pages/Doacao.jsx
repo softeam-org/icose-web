7e746d30985b79ee498b68b6a3f0242b99d497ea
Uneto47 <netovasconcelos02@gmail.com>
2023-08-30 11:50:11 -0300

    Atualizando a responsividade da pagina Doacao

--- src/Pages/Doacao.jsx
+++ src/Pages/Doacao.jsx
@@ -1,7 +1,5 @@
 import womenWorkin from "@/assets/women-working.svg"
-import PixBg from "@/components/Donation/PixBg";
-
-
+import PixBg from "../components/Donation/PixBg";
 
 function Donation() {
   return (
@@ -13,7 +11,7 @@ function Donation() {
           alt="Mulher trabalhando em serviço comunitário"
         />
         <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-40 xl:w-full xl:h-[40rem]">
-          <p className="text-white text-lg md:text-xl lg:text-2xl xl:text-6xl	font-bold px-6 w-3/4 h-4/6 tracking-wide xl:leading-tight">
+          <p className="text-white text-2xl md:text-4xl lg:text-4xl xl:text-6xl	font-bold px-6 w-3/4 h-4/6 tracking-wide leading-tight">
             Todo valor arrecadado no território em 2023 será integralmente repassado para iniciativas selecionadas através do mapeamento e/ou edital.
           </p>
         </div>
