48e9781cb709f1423c3854b72bd8aed905a06ab6
Jeferson-Dcomp <jefersondeoliveira062@gmail.com>
2023-08-31 00:50:21 -0300

    Adiciona lib Swiper para carrossel, remove configurações antigas da DaisyUI e modifica arquivo de teste news.js

--- data/news.js
+++ data/news.js
@@ -1,117 +1,36 @@
 [
-  {
-    "id": "slide 1",
-    "cards": [
-      {
-        "image": "src/assets/news.svg",
-        "title": "Título da matéria",
-        "date": "10 de agosto de 2023",
-        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
-      },
-      {
-        "image": "src/assets/news.svg",
-        "title": "Título da matéria",
-        "date": "10 de agosto de 2023",
-        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
-      },
-      {
-        "image": "src/assets/news.svg",
-        "title": "Título da matéria",
-        "date": "10 de agosto de 2023",
-        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
-      }
-    ]
+  { "id" : 1, 
+    "image": "src/assets/news.svg",
+    "title": "Título da matéria",
+    "date": "10 de agosto de 2023",
+    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
   },
   {
-    "id": "slide 2",
-    "cards": [
-      {
-        "image": "src/assets/news.svg",
-        "title": "Título da matéria",
-        "date": "10 de agosto de 2023",
-        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
-      },
-      {
-        "image": "src/assets/news.svg",
-        "title": "Título da matéria",
-        "date": "10 de agosto de 2023",
-        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
-      },
-      {
-        "image": "src/assets/news.svg",
-        "title": "Título da matéria",
-        "date": "10 de agosto de 2023",
-        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
-      }
-    ]
+    "id" : 2, 
+    "image": "src/assets/news.svg",
+    "title": "Título da matéria",
+    "date": "10 de agosto de 2023",
+    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
   },
   {
-    "id": "slide 3",
-    "cards": [
-      {
-        "image": "src/assets/news.svg",
-        "title": "Título da matéria",
-        "date": "10 de agosto de 2023",
-        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
-      },
-      {
-        "image": "src/assets/news.svg",
-        "title": "Título da matéria",
-        "date": "10 de agosto de 2023",
-        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
-      },
-      {
-        "image": "src/assets/news.svg",
-        "title": "Título da matéria",
-        "date": "10 de agosto de 2023",
-        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
-      }
-    ]
+    "id" : 3, 
+    "image": "src/assets/news.svg",
+    "title": "Título da matéria",
+    "date": "10 de agosto de 2023",
+    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
   },
   {
-    "id": "slide 4",
-    "cards": [
-      {
-        "image": "src/assets/news.svg",
-        "title": "Título da matéria",
-        "date": "10 de agosto de 2023",
-        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
-      },
-      {
-        "image": "src/assets/news.svg",
-        "title": "Título da matéria",
-        "date": "10 de agosto de 2023",
-        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
-      },
-      {
-        "image": "src/assets/news.svg",
-        "title": "Título da matéria",
-        "date": "10 de agosto de 2023",
-        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
-      }
-    ]
+    "id" : 4, 
+    "image": "src/assets/news.svg",
+    "title": "Título da matéria",
+    "date": "10 de agosto de 2023",
+    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
   },
-  {
-    "id": "slide 5",
-    "cards": [
-      {
-        "image": "src/assets/news.svg",
-        "title": "Título da matéria",
-        "date": "10 de agosto de 2023",
-        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
-      },
-      {
-        "image": "src/assets/news.svg",
-        "title": "Título da matéria",
-        "date": "10 de agosto de 2023",
-        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
-      },
-      {
-        "image": "src/assets/news.svg",
-        "title": "Título da matéria",
-        "date": "10 de agosto de 2023",
-        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
-      }
-    ]
-  }
+  { "id" : 5, 
+  "image": "src/assets/news.svg",
+  "title": "Título da matéria",
+  "date": "10 de agosto de 2023",
+  "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
+}
+
 ]
--- src/components/Inicio/Carousel.jsx
+++ src/components/Inicio/Carousel.jsx
@@ -1,112 +1,68 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { CaretCircleLeft, CaretCircleRight } from "@phosphor-icons/react";
+import React, { useState, useEffect } from 'react';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
+
 
 function Carousel() {
-  const [currentIndex, setCurrentIndex] = useState(0);
   const [data, setData] = useState([]);
-  const carousel = useRef(null);
 
   useEffect(() => {
+    
     const fetchData = async () => {
       try {
         const response = await fetch('http://localhost:5173/data/news.js');
         const responseData = await response.json();
         setData(responseData);
+        console.log(data)
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
 
     fetchData();
-  }, []);
+  }, []);  
 
-  useEffect(() => {
-// Here is the logic to get the scroll size
-    if (carousel.current) {
-      carousel.current.scrollLeft = currentIndex * carousel.current.offsetWidth;
-    }
-  }, [currentIndex]);
+    if (!data || !data.length) return null;
 
-//Here the scroll position is set and passed as index. In this way it is possible to assign the conditional of the labels.
-  const handleCarouselScroll = () => {
-    if (carousel.current) {
-      const scrollPosition = carousel.current.scrollLeft;
-      const itemWidth = carousel.current.offsetWidth;
-      const newIndex = Math.round(scrollPosition / itemWidth);
-      setCurrentIndex(newIndex);
-    }
-  };
 
-  if (!data || !data.length) return null;
-
-// Here is the logic next on the slide and previous on the slides
-  const handleLeftClick = (e) => {
-    e.preventDefault();
-    carousel.current.scrollLeft -= carousel.current.offsetWidth;
-  };
-
-  const handleRightClick = (e) => {
-    e.preventDefault();
-    carousel.current.scrollLeft += carousel.current.offsetWidth;
-  };
-
-  return (
-    
-    <div className='flex flex-col'>
-      <div className='flex flex-row justify-center items-center space-x-5'>
-			<div className='w-10'>
-        <div className='w-9'>
-          <CaretCircleLeft
-            size={32}
-            onClick={handleLeftClick}
-            className='w-8 hover:w-9 hover:h-9 hover:fill-standard-blue  cursor-pointer'
-          />
-         </div>
-				</div>
-        <div className="carousel carousel-center w-10/12 flex py-8 space-x-10" ref={carousel} onScroll={handleCarouselScroll}>
-          {data.map((slide, slideIndex) => (
-            <div id={slide.id} className="flex flex-row carousel-item w-full space-x-10" key={slideIndex}>
-              {slide.cards.map((card, cardIndex) => (
-                <div className="w-1/3 " key={cardIndex}>
-                  <img src={card.image} className="w-full" />
-                  <div className="w-full flex flex-col space-y-1.5 md:space-y-2 py-3 ">
-                    <h2 className="font-inter font-semibold text-standard-blue text-xs sm:text-base md:text-2xl xl:text-2xl text-left">
-                      {card.title}
-                    </h2>
-                    <p className="text-xs">{card.date}</p>
-                    <p className="text-xs sm:text-sm md:text-base w-full h-full ">
-                      {card.content}
-                    </p>
-                  </div>
-                </div>
-              ))}
+return(
+  <div>
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      navigation
+      pagination={{ clickable: true }}
+    >
+            
+      {data.map((slide) => (
+      
+        <SwiperSlide key={slide.id} className='w-1/3'>
+          
+            <img src={slide.image} className="w-full" />
+            <div className="w-full flex flex-col space-y-1.5 md:space-y-2 py-3 ">
+              <h2 className="font-inter font-semibold text-standard-blue text-xs sm:text-base md:text-2xl xl:text-2xl text-left">
+                {slide.title}
+              </h2>
+              <p className="text-xs">{slide.date}</p>
+              <p className="text-xs sm:text-sm md:text-base w-full h-full ">
+                {slide.content}
+              </p>
             </div>
-          ))}
-        </div>
-        <div className='w-9'>
-          <CaretCircleRight
-            size={32}
-            onClick={handleRightClick}
-            className='w-8 hover:w-9 hover:h-9 hover:fill-standard-blue  cursor-pointer'
-          />
-        </div>
-				
-      </div>
+           
+          </SwiperSlide>  
+      ))}
 
-      {/* Radio buttons */}
-      <div className="flex justify-center w-full h-2 gap-2">
-        {data.map((_, slideIndex) => (
-          <span
-            key={slideIndex}
-            className={` w-2 h-2 rounded-full ${
-              currentIndex === slideIndex ? 'bg-black' : 'bg-white border border-solid border-black'
-            }`}
-          >
-          </span>
-        ))}
-      </div>
-    </div>
+    </Swiper>
+  </div>
+  
   );
-};
+}
 
 export default Carousel;
