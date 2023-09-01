de60d745e5375547f546fa1ba974cac8607dae11
Jeferson-Dcomp <jefersondeoliveira062@gmail.com>
2023-09-01 01:03:33 -0300

    atualiza configurações do carrosel

--- data/news.js
+++ data/news.js
@@ -1,5 +1,6 @@
 [
-  { "id" : 1, 
+  {
+    "id" : 1, 
     "image": "src/assets/news.svg",
     "title": "Título da matéria",
     "date": "10 de agosto de 2023",
@@ -26,11 +27,12 @@
     "date": "10 de agosto de 2023",
     "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
   },
-  { "id" : 5, 
-  "image": "src/assets/news.svg",
-  "title": "Título da matéria",
-  "date": "10 de agosto de 2023",
-  "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
-}
+  { 
+    "id" : 5, 
+    "image": "src/assets/news.svg",
+    "title": "Título da matéria",
+    "date": "10 de agosto de 2023",
+    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et turpis non metus interdum rutrum..."
+  }
 
 ]
--- src/components/Inicio/Carousel.jsx
+++ src/components/Inicio/Carousel.jsx
@@ -1,14 +1,11 @@
 import React, { useState, useEffect } from 'react';
-
+//imports swiper
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// import Swiper and modules styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-
-
-
+import 'swiper/css/scrollbar';
+import {Pagination, Navigation } from 'swiper/modules';
 
 function Carousel() {
   const [data, setData] = useState([]);
@@ -20,48 +17,53 @@ function Carousel() {
         const response = await fetch('http://localhost:5173/data/news.js');
         const responseData = await response.json();
         setData(responseData);
-        console.log(data)
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
-
     fetchData();
   }, []);  
 
     if (!data || !data.length) return null;
-
-
-return(
-  <div>
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      navigation
-      pagination={{ clickable: true }}
-    >
-            
+  return(
+    <div>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        pagination={{
+          dynamicBullets: true,
+        }}
+        navigation ={true}
+        //configurando setas manualmente
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          }
+        }}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >   
       {data.map((slide) => (
-      
-        <SwiperSlide key={slide.id} className='w-1/3'>
-          
-            <img src={slide.image} className="w-full" />
-            <div className="w-full flex flex-col space-y-1.5 md:space-y-2 py-3 ">
-              <h2 className="font-inter font-semibold text-standard-blue text-xs sm:text-base md:text-2xl xl:text-2xl text-left">
-                {slide.title}
-              </h2>
-              <p className="text-xs">{slide.date}</p>
-              <p className="text-xs sm:text-sm md:text-base w-full h-full ">
-                {slide.content}
-              </p>
-            </div>
-           
+        <SwiperSlide key={slide.id} className='w-80 pb-16'>
+            <img src={slide.image} className=" w-full" />
+              <div className="w-full flex flex-col space-y-1.5 md:space-y-2 py-3 ">
+                <h2 className="font-inter font-semibold text-standard-blue text-xs sm:text-base md:text-2xl xl:text-2xl text-left">
+                  {slide.title}
+                </h2>
+                <p className="text-xs">{slide.date}</p>
+                <p className="text-xs sm:text-sm md:text-base w-full h-full ">
+                  {slide.content}
+                </p>
+            </div> 
           </SwiperSlide>  
       ))}
-
-    </Swiper>
-  </div>
-  
+      </Swiper>
+    </div>
   );
 }
 
