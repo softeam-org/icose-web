e7824a20f9dd56f73a67c49872f3816f22079bad
Daniel Sena <45214080+DanielSenaDev@users.noreply.github.com>
2023-09-14 11:32:55 -0300

    Remove codigo nao utilizado

--- src/Pages/Comunitario.jsx
+++ src/Pages/Comunitario.jsx
@@ -5,12 +5,6 @@ import { MapPinLine } from "@phosphor-icons/react";
 function Community() {
   return (
     <div className="flex-1 text-2xl pb-24">
-
-      
-      <section className="flex flex-col px-10 py-10 gap-2 "> 
-        
-      </section>
-
       <section>
         <div className="flex flex-col gap-10 xl:gap-20 pb-10 pt-10 xl:pt-20 mx-auto max-w-[100rem] px-12">
 
