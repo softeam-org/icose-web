4e144c5ab6b86464cd6c8e40519881ba6d67de4a
Daniel Sena <danielsan.2000@hotmail.com>
2023-09-14 11:30:56 -0300

    Atualiza posição do elemento do commit anterior

--- src/Pages/Comunitario.jsx
+++ src/Pages/Comunitario.jsx
@@ -6,22 +6,26 @@ function Community() {
   return (
     <div className="flex-1 text-2xl pb-24">
 
-      {/* Componetizar essa seção/elemento abaixo */}
+      
       <section className="flex flex-col px-10 py-10 gap-2 "> 
-        <div className="collapse collapse-plus text-white bg-standard-blue">
-          <input type="checkbox" className="accordion-1" />
-          <div className="collapse-title text-xl font-medium flex flex-row gap-5">
-            <MapPinLine size={24} />
-            <span>TITULO</span>
-          </div>
-          <div className="collapse-content text-base px-10">
-            <p>Descricao</p>
-          </div>
-        </div>
+        
       </section>
 
       <section>
         <div className="flex flex-col gap-10 xl:gap-20 pb-10 pt-10 xl:pt-20 mx-auto max-w-[100rem] px-12">
+
+          {/* --- Componetizar elemento abaixo --- */}
+          <div className="collapse collapse-plus text-white bg-standard-blue">
+            <input type="checkbox" className="accordion-1" />
+            <div className="collapse-title text-xl font-medium flex flex-row gap-5">
+              <MapPinLine size={24} />
+              <span>TITULO</span>
+            </div>
+            <div className="collapse-content text-base px-10">
+              <p>Descricao</p>
+            </div>
+          </div>
+          {/* ------ */}
           < NacionalCommunity />
         </div>
       </section>    
