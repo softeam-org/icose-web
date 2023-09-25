93ac1e02d5edbcb69e3e73181af48cb1bedf3f64
Marcelo Mota <marcelomotadev@gmail.com>
2023-09-25 11:42:28 -0300

    Componentiza sliders

--- src/Pages/Comunitario.jsx
+++ src/Pages/Comunitario.jsx
@@ -1,11 +1,30 @@
 import NationalCommunity from "@/components/Comunitario/NationalCommunity";
-import { MapPinLine } from "@phosphor-icons/react";
+import Title from "@/components/Title";
+import SliderC from "@/components/Comunitario/sliderCommunity";
+
+
 
 function Community() {
   return (
     <div className="flex-1 text-2xl pb-24">
+
       <section>
-        <div className="flex flex-col gap-10 xl:gap-20 pb-10 pt-10 xl:pt-20 mx-auto max-w-[100rem] px-12">
+      <div className="flex flex-col gap-10 xl:gap-14 pb-10 pt-10 xl:pt-20 mx-auto max-w-[100rem] px-12">
+        <Title 
+        color="standard-blue"
+        content="Institutos Comunitários"
+        />
+        <SliderC icon="mapPinLine" title="Instituições locais" content="As organizações dedicam-se à melhoria da qualidade de vida de comunidades situadas em uma região geográfica bem delimitada, da qual são originárias e na qual estão estabelecidas."/>
+        <SliderC icon="handShake" title="Provedoras de apoio institucional e técnico às organizações e iniciativas sociais locais" content="Responsáveis por impulsionar o desenvolvimento e a construção de capacidades das organizações da sociedade civil e iniciativas sociais locais, de modo a elevar padrões de operação e garantir o uso responsável e eficiente dos recursos doados.
+        "/>
+        <SliderC icon="hexagon" title="Multitemáticas" content="Apoiam e investem em outras organizações da sociedade civil e iniciativas sociais de modo a abranger a diversidade de causas e temas relevantes para a comunidade, seu contexto e suas demandas próprias."/>
+        <SliderC icon="" title="" content=""/>
+        <SliderC icon="" title="" content=""/>
+      </div>
+      </section>
+
+      <section>
+        <div className="flex flex-col gap-10 xl:gap-14 pb-10 pt-10 xl:pt-20 mx-auto max-w-[100rem] px-12">
         <Title 
         color="#F59E0B"
         content="Institutos Comunitários no Brasil"
--- src/components/Comunitario/NationalCommunity.jsx
+++ src/components/Comunitario/NationalCommunity.jsx
@@ -1,13 +1,9 @@
 import Participantes from "@/assets/MapaFIC_ProgramaTT.png";
-import Title from "@/components/Title";
 
-function NacionalCommunity() {
+
+function NationalCommunity() {
   return (
     <div className="flex flex-col gap-6 xl:gap-8 text-black"> 
-        <Title 
-        color="#F59E0B"
-        content="Institutos Comunitários no Brasil"
-        />
         <p className="font-bold ">
           Até pouco tempo atrás, existiam no Brasil apenas três organizações que operavam neste modelo: o Instituto Comunitário Grande Florianópolis, o Tabôa - Desenvolvimento Comunitário e o Instituto Baixada Maranhense.  Porém, desde 2018 o Instituto para o Desenvolvimento do Investimento Social (IDIS), em parceria com a Charles Stewart Mott Foundation, criou o programa Transformando Territórios, que vem fomentando a criação de mais Institutos e Fundações Comunitárias no Brasil.
         </p>
@@ -16,4 +12,4 @@ function NacionalCommunity() {
   );
 }
 
-export default NacionalCommunity;
+export default NationalCommunity;
