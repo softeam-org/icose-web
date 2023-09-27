11de8478580fa25b0a0da469bf2b2a0d4e91f62e
Carlos Adriano <cadrianogamac@gmail.com>
2023-09-27 11:10:26 -0300

    Corrige indentação

--- src/components/About/Transparency/Transparency.jsx
+++ src/components/About/Transparency/Transparency.jsx
@@ -4,15 +4,15 @@ function Transparency() {
   return(
     <div className="flex flex-col space-y-10">
       <Title
-      color = "#365314"
-      content={"Transparência"}
+        color = "#365314"
+        content="Transparência"
       />  
       <div className="space-y-5">
         <p className="text-xl">Transparência é um dos valores do ICOSE. Acreditamos que prestações de contas claras e acessíveis contribuem para a  construção de relações duradouras baseadas na confiança e credibilidade.</p>
         <p className="text-xl">Não basta ser honesto, temos que mostrar que somos honestos!</p>
         <p className="text-xl">Demonstrativos financeiros e de atividades serão postados aqui.</p>
       </div>
-  </div>
+    </div>
   );
 }
 export default Transparency;
