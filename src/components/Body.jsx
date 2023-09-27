de55ad8815bea179a9909c93b0fd5f10f7f3a205
Carlos Adriano <cadrianogamac@gmail.com>
2023-09-27 12:06:31 -0300

    Cria componente genérico o conteúdo das páginas

---
+++ src/components/Body.jsx
@@ -0,0 +1,9 @@
+const Body = ({ children }) => {
+  return (
+    <div className="pb-10 pt-10 xl:pt-20 xl:pb-20 mx-auto max-w-[80rem] px-12">
+      {children}
+    </div>
+  );
+};
+
+export default Body;
