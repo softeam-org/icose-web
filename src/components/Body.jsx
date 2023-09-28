29d8a3d834ff3e8f30d0016c312957725a9efa8b
Ádrian Gama <adrian.gama.se@gmail.com>
2023-09-27 21:35:26 -0300

    Remove espaçamento em telas grandes no Body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,6 +1,6 @@
 const Body = ({ children }) => {
   return (
-    <div className="pb-10 pt-10 xl:pt-20 xl:pb-20 mx-auto max-w-[80rem] px-12">
+    <div className="pb-10 pt-10 mx-auto max-w-[80rem] px-12">
       {children}
     </div>
   );
