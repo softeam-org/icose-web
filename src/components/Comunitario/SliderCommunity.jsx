89e5253fed9c8f143524be09b875996072e93d47
Carlos Adriano <cadrianogamac@gmail.com>
2023-09-28 23:37:41 -0300

    Remove classes desnecessárias

--- src/components/Comunitario/SliderCommunity.jsx
+++ src/components/Comunitario/SliderCommunity.jsx
@@ -3,7 +3,7 @@ function SliderCommunity({ icon, title, content }) {
     <div className="collapse collapse-arrow text-white bg-standard-blue">
       <input type="radio" name="community-accordion" />
       <div className="collapse-title text-xl font-medium flex flex-row gap-5">
-        <span className="flex flex-col justify-center">{icon}</span>  
+        <span>{icon}</span>  
         <span className="mx-auto uppercase">{title}</span>
       </div>
       <div className="collapse-content text-base px-16">
