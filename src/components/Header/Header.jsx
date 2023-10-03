58c7b3145290e2e15fb0ab74a7fcada3051465d5
Carlos Adriano <cadrianogamac@gmail.com>
2023-10-03 18:01:43 -0300

    Altera responsividade do padding lateral do Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import HeaderLogo from "@/components/Header/Logo";
 function Header() {
   return (
     <div className="fixed z-50 inset-x-0 top-0 bg-standard-blue">
-      <div className="sm:mx-12 mx-6 py-3 flex gap-4 justify-between items-center">
+      <div className="lg:px-12 px-6 py-3 flex gap-4 justify-between items-center">
         <HeaderLogo />
         <NavBar />
       </div>
