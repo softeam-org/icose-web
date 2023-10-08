01ad2bf1fcbd13f93d9734edf9b33dda76b77634
Carlos Adriano <cadrianogamac@gmail.com>
2023-10-07 22:18:19 -0300

    Refatora componentes relacionados ao componente `Project`

---
+++ src/components/Projects/AtividadesSolidarias.jsx
@@ -0,0 +1,11 @@
+function AtividadesSolidarias() {
+  return (
+    <p> 
+      Trata-se de uma adaptação da Maratona da Solidariedade a Jornada da Criança realizada numa escola particular em Aracaju.
+      <br/>
+      Substitui-se a caminhada por atividades diversificadas na quadra esportiva da escola
+    </p>
+  );
+}
+
+export default AtividadesSolidarias;
---
+++ src/components/Projects/ConhecendoTerritorio.jsx
@@ -0,0 +1,12 @@
+function ConhecendoTerritorio() {
+  return (
+    <p> 
+      Para identificar o potencial e as necessidades, estamos realizando o mapeamento das
+      ONGs e iniciativas individuais no estado de Sergipe. Estas informações serão essenciais para
+      a construção de futuros editais e/ou repasse de recursos. Participe! 
+    </p>
+  );
+}
+
+export default ConhecendoTerritorio;
+
