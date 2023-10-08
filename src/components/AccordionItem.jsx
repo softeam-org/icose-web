b8d3c463de57e9dd61cf00a614421e6df8fa7a73
Carlos Adriano <cadrianogamac@gmail.com>
2023-10-07 22:20:16 -0300

    Refatora componente `AccordionItem`

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -2,13 +2,13 @@ import setColor from "@/utils/setColor"
 
 function AccordionItem({ icon=null, bgColor, textColor="#FFFFFF", title, children }) {
   return (
-    <div style={{ backgroundColor: `${setColor(bgColor)}`, color: `${setColor(textColor)}`}} className="collapse collapse-arrow">
+    <div style={{ backgroundColor: `${setColor(bgColor)}`, color: `${setColor(textColor)}`}} className="collapse collapse-arrow shadow-lg">
       <input type="radio" name="accordion" />
       <div className="collapse-title text-xl font-medium flex flex-row gap-5">
         <span>{icon}</span>  
-        <span className="mx-auto uppercase">{title}</span>
+        <span className="uppercase font-bold">{title}</span>
       </div>
-      <div className="collapse-content text-base px-16">
+      <div className="collapse-content text-xl px-16">
         {children}
       </div>
     </div>
