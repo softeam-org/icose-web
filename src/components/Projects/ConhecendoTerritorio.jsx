7e5d6673d423a0c2de41bdda03bcb88d9af113c3
Jorge Mendonça Boaretto Loch <jorgembloch2001@gmail.com>
2023-10-08 15:40:33 -0300

    modifica componente ConhecendoTerritorio

--- src/components/Projects/ConhecendoTerritorio.jsx
+++ src/components/Projects/ConhecendoTerritorio.jsx
@@ -1,10 +1,15 @@
 function ConhecendoTerritorio() {
   return (
-    <p> 
-      Para identificar o potencial e as necessidades, estamos realizando o mapeamento das
-      ONGs e iniciativas individuais no estado de Sergipe. Estas informações serão essenciais para
-      a construção de futuros editais e/ou repasse de recursos. Participe! 
-    </p>
+    <div className="space-y-2">
+      <p> 
+        Para identificar o potencial e as necessidades, estamos realizando o mapeamento das
+        ONGs e iniciativas individuais no estado de Sergipe. Estas informações serão essenciais para
+        a construção de futuros editais e/ou repasse de recursos. Participe! 
+      </p>
+      <p>
+        Em breve, link para inscrição.
+      </p>
+    </div>
   );
 }
 
